90ff167fded7068874fac2e73bfd6cf7c740c3ac
Jose Oliveira <jedpfo89@gmail.com>
2025-03-04 15:55:18 +0000

    .

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -13,6 +13,30 @@ class TasksController < ApplicationController
   end
 
   def create
+    # raise
+    Task.create(task_params)
+    redirect_to tasks_path
+  end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
